48c3a16d79087fd3bd751d38da955039f54807d4
viskas24 <107520741+viskas24@users.noreply.github.com>
2023-09-03 22:08:09 +0300

    Create PCtoE10.py

---
+++ PCtoE10.py
@@ -0,0 +1,37 @@
+#I'm sorry for bad code
+
+
+import socket
+
+file = open ('TEST.sicmf', 'rb') # File you need to copy to SIC controller
+
+line=file.readline()
+
+
+HOST = '192.168.1.79'    # The remote host
+PORT = 8899              # The same port as used by the server
+
+print(line)
+l=len(line)
+print(l)                # length for sicnf file
+
+l=l+13
+h=hex(l)
+print(h)                # length + 13 (filename) in hex 
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+#    s.sendall(b'\x02\x00\x35\x53\x00\x0D'+b'TEST'+b'\x00\x00\x00\x00\x00\x00\x00\x00\x02\x03')    # load file from e10
+#    data: bytes = s.recv(1024)
+#    print('Received', repr(data))      # test message from e10
+#    data = data[17:77]
+#    print('File', repr(data))
+
+    s.sendall(b'\x02\x00\x35\x47\x00\x5c' + b'SAMPLE' + b'\x00\x00\x00\x00\x00\x00\x02'+ line + b'\x03') # load file to e10
+
+#   \x00\x5c - change to your 'h'
+#   length fo filename =11. unused letter = \x00
+#  SAMPLE = 6 letter + 5'\x00' + fixed '\x00\x02'
+
+    data = s.recv(1024)
+    print('Received', repr(data))      # test message from e10
