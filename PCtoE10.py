d19590855d6b6be047ea49b708bbd7e1a42354f8
viskas24 <107520741+viskas24@users.noreply.github.com>
2023-09-03 22:10:48 +0300

    Update PCtoE10.py

--- PCtoE10.py
+++ PCtoE10.py
@@ -13,7 +13,7 @@ PORT = 8899              # The same port as used by the server
 
 print(line)
 l=len(line)
-print(l)                # length for sicnf file
+print(l)                # length for sicmf file
 
 l=l+13
 h=hex(l)
@@ -21,17 +21,12 @@ print(h)                # length + 13 (filename) in hex
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-#    s.sendall(b'\x02\x00\x35\x53\x00\x0D'+b'TEST'+b'\x00\x00\x00\x00\x00\x00\x00\x00\x02\x03')    # load file from e10
-#    data: bytes = s.recv(1024)
-#    print('Received', repr(data))      # test message from e10
-#    data = data[17:77]
-#    print('File', repr(data))
-
     s.sendall(b'\x02\x00\x35\x47\x00\x5c' + b'SAMPLE' + b'\x00\x00\x00\x00\x00\x00\x02'+ line + b'\x03') # load file to e10
 
-#   \x00\x5c - change to your 'h'
-#   length fo filename =11. unused letter = \x00
-#  SAMPLE = 6 letter + 5'\x00' + fixed '\x00\x02'
+#   '\x02\x00\x35\x47' - fixed command
+#   '\x00\x5c' - change to your 'h'
+#    length for filename =11. unused letter = \x00
+#    SAMPLE = 6 letter + 5'\x00' + fixed '\x00\x02'
 
     data = s.recv(1024)
     print('Received', repr(data))      # test message from e10
